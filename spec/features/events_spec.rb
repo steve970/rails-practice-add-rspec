a5517e2cc680bacb4146d7c0ceead113141468aa
Steve Hirschhorn <hirschhorn970@gmail.com>
2015-02-19 15:22:32 -0700

    added some more to my rspec

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 feature 'creates Events' do
   scenario 'User creates a new event' do
-
     visit root_path
     click_on 'New Event'
     fill_in 'Description', with: 'event.description'
@@ -11,11 +10,13 @@ feature 'creates Events' do
     fill_in 'Capacity', with: 'event.capacity'
     check 'Requires ID'
     click_on 'Create Event'
+    expect(page).to have_content('event.description')
   end
 
   scenario 'Edits the Event' do
     Event.create(description:'gSchool')
     visit root_path
+    expect(page).to have_content('gSchool')
     click_on 'edit'
     fill_in 'Description', with: 'event.description'
     fill_in 'Location', with: 'event.location'
@@ -23,6 +24,8 @@ feature 'creates Events' do
     fill_in 'Capacity', with: 'event.capacity'
     check 'Requires ID'
     click_on 'Update Event'
+    expect(page).to have_content('event.description')
+    expect(page).to have_no_content('gSchool')
   end
 
   scenario 'Shows the Event' do
@@ -31,10 +34,11 @@ feature 'creates Events' do
     click_on 'gSchool'
     click_on 'Show all events'
   end
-  
+
   scenario 'Deletes the Event' do
     Event.create(description:'gSchool')
     visit root_path
     click_on 'delete'
+    expect(page).to have_no_content('gSchool')
   end
 end
